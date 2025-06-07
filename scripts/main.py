adaee6ba8546d514f322ac988236740abac07a21
VicmanGT <giltafollavictormanuel@gmail.com>
2025-06-06 18:24:17 -0600

    Added files for inverse kinematics in scl for s7 1200 plc

--- scripts/main.py
+++ scripts/main.py
@@ -7,15 +7,15 @@ board = [[" " for _ in range(3)] for _ in range(3)]
 list_board = [cell for row in board for cell in row]
 
 BOARD_COORIDNATES = {
-    '0': {'x': -90.0, 'y': -30.0},
-    '1': {'x': -20.0, 'y': 20.0},
-    '2': {'x': 45.0, 'y': 55.0},
-    '3': {'x': -130.0, 'y': 40.0},
-    '4': {'x': -65.0, 'y': 95.0},
-    '5': {'x': 5.0, 'y': 130.0},
-    '6': {'x': -170.0, 'y': 110.0},
-    '7': {'x': -100.0, 'y': 150.0},
-    '8': {'x': -35.0, 'y': 210.0},
+    '0': {'x': -100.0, 'y': -15.0},
+    '1': {'x': -25.0, 'y': 25.0},
+    '2': {'x': 40.0, 'y': 70.0},
+    '3': {'x': -140.0, 'y': 50.0},
+    '4': {'x': -70.0, 'y': 100.0},
+    '5': {'x': -5.0, 'y': 145.0},
+    '6': {'x': -175.0, 'y': 120.0},
+    '7': {'x': -105.0, 'y': 170.0},
+    '8': {'x': -40.0, 'y': 220.0},
 }
 
 FREE_TOKEN_COORDINATES = {
@@ -27,7 +27,21 @@ FREE_TOKEN_COORDINATES = {
     }
 
 z_high = -400.0
-z_low = -530.0
+
+Z_LOW = {
+    '0': -520.0,
+    '1': -520.0,
+    '2': -505.0,
+    '3': -505.0,
+    '4': -495.0
+}
+z_1_low = -522.0
+z_2_low = -522.0
+z_3_low = -505.0
+z_4_low = -505.0
+z_5_low = -495
+
+z_low = - 500
 #print("Cognex Data:", data)
 print()
 
@@ -55,6 +69,7 @@ while not game.game_over():
     #my_turn = 1  # For testing purposes, we assume it's always our turn
     if my_turn == 1 and previous_turn_state == 0 and not waiting_for_off:
         print("It's my turn!")
+        time.sleep(1)  # Wait for a moment to ensure the PLC signal is stable
         done_turn = False
         
         data, unused_token_coordinates = client_cognex.get_cognex_data()
@@ -116,7 +131,7 @@ while not game.game_over():
         token_x = FREE_TOKEN_COORDINATES[str(token)]['x']
         token_y = FREE_TOKEN_COORDINATES[str(token)]['y']
 
-        token += 1
+        
 
         print("Current Board State:")
         game.print_board()
@@ -131,8 +146,8 @@ while not game.game_over():
         plc_connection.activate_start_play()
         time.sleep(4)
         # Go down
-        print(f"Moving to Z low: {z_low}")
-        plc_connection.write_oef__plc(token_x, token_y, z_low)
+        print(f"Moving to Z low: {Z_LOW[str(token)]}")
+        plc_connection.write_oef__plc(token_x, token_y, Z_LOW[str(token)])
         plc_connection.activate_start_play()
         time.sleep(4)
 
@@ -176,7 +191,7 @@ while not game.game_over():
         print("Going home")
         plc_connection.activate_conveyor()
         plc_connection.write_go_home_plc()
-        time.sleep(4)
+        time.sleep(6)
         plc_connection.deactivate_conveyor()
     elif my_turn == 0 and waiting_for_off:
         # Signal turned off, we can reset and wait for next rising edge
@@ -185,6 +200,11 @@ while not game.game_over():
     
     # Update previous state
     previous_turn_state = my_turn
+
+    token += 1
+
+    if token > 4:
+        token = 0
 
 print("Game Over!")
 
